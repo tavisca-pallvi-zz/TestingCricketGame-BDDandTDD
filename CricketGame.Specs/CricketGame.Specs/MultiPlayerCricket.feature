4037b720b87f8e88f87a0e6da99fb5768e454072
pallvi <goelpallvi21@gmail.com>
2018-08-09 17:36:44 +0530

    Final assignment

---
+++ CricketGame.Specs/CricketGame.Specs/MultiPlayerCricket.feature
@@ -0,0 +1,73 @@
+﻿Feature: MultiPlayerCricket
+	In order to enjoy the multiplayer cricket competition
+	As a player
+	I want to be told the winner of game
+
+@mytag
+    Scenario: PlayerFirst and PlayerSecond score is zero when game starts	
+	When  PlayerFirst and PlayerSecond starts a game of cricket
+	Then  PlayerFirst and PlayerSecond scores should be 0
+
+
+	Scenario: PlayerFirst should be able to score runs	
+	Given  Cricket game has started
+	When PlayerFirst scores 4 runs
+	Then  PlayerFirst scores should be 4
+
+	Scenario: PlayerSecond should be able to score runs	
+	Given  Cricket game has started
+	When PlayerSecond scores 3 runs
+	Then  PlayerSecond scores should be 3
+
+
+	
+	Scenario: PlayerFirst should be able to score multiple runs	
+	Given  Cricket game has started
+	And PlayerFirst has scored 4 runs
+    When PlayerFirst scores 3 runs
+	Then  PlayerFirst scores should be 7
+
+	
+	
+	Scenario: PlayerSecond should be able to score multiple runs	
+	Given  Cricket game has started
+	And PlayerSecond has scored 3 runs
+	When PlayerSecond scores 6 runs
+	Then  PlayerSecond scores should be 9
+
+
+
+	Scenario: PlayerFirst should not be able to score runs after getting out
+	Given  Cricket game has started
+	And PlayerFirst has scored 4 runs
+	And PlayerFirst gets out
+
+	When  PlayerFirst scores 3 runs
+	Then  PlayerFirst scores should be 4
+
+	Scenario: PlayerSecond should not be able to score runs after getting out
+	Given  Cricket game has started
+	And PlayerSecond has scored 3 runs
+	And PlayerSecond gets out
+	When  PlayerSecond scores 6 runs
+	Then  PlayerSecond scores should be 3
+
+
+	Scenario: PlayerSecond is winner
+	Given  Cricket game has started
+	And PlayerSecond has scored 9 runs
+	And PlayerFirst has scored  6 runs
+	Then  PlayerSecond is winner
+
+	
+	Scenario: PlayerFirst is winner
+	Given  Cricket game has started
+	And PlayerFirst has scored 6 runs
+	And PlayerSecond has scored 3 runs
+	Then PlayerFirst is winner
+
+    Scenario: Match is tie
+	Given  Cricket game has started
+	And PlayerFirst has scored 6 runs
+	And PlayerSecond has scored 6 runs
+	Then  Match is tie
---
+++ CricketGame.Specs/CricketGame.Specs/MultiPlayerCricketSteps.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using CricketGame;
+using FluentAssertions;
+
+namespace CricketGame.Specs
+{
+    [Binding]
+
+    public class MultiPlayerCricketSteps
+    {
+        private Cricket _playerFirst=null, _playerSecond=null;
+      
+
+        [When(@"PlayerFirst and PlayerSecond starts a game of cricket")]
+        [Given(@" Cricket game has started")]
+        public void When_playerFirstAndPlayerSecondStartsAGameOfCricket()
+        {
+            _playerFirst = new Cricket();
+            _playerSecond= new Cricket();
+
+        }
+        
+        [Then(@"PlayerFirst and PlayerSecond scores should be (.*)")]
+        public void ThenPlayerFirstAndPlayerSecondScoresShouldBe(int scores)
+        {
+            _playerFirst.PlayerScore.Should().Be(scores);
+            _playerSecond.PlayerScore.Should().Be(scores);
+        }
+
+       
+        [When(@"PlayerFirst scores (.*) runs")]
+        public void WhenPlayerFirstScoresRuns(int scores)
+        {
+           
+            _playerFirst.Score(scores);
+        }
+
+        [Then(@"PlayerFirst scores should be (.*)")]
+        public void ThenPlayerFirstScoresShouldBe(int scores)
+        {
+            _playerFirst.PlayerScore.Should().Be(scores);
+        }
+
+      
+        [When(@"PlayerSecond scores (.*) runs")]
+        public void WhenPlayerSecondScoresRuns(int scores)
+        {
+            
+                _playerSecond.Score(scores);
+        }
+
+        [Then(@"PlayerSecond scores should be (.*)")]
+        public void ThenPlayerSecondScoresShouldBe(int scores)
+        {
+           _playerSecond.PlayerScore.Should().Be(scores);
+        }
+
+        [Given(@"Cricket game has started")]
+        public void GivenCricketGameHasStarted()
+        {
+            _playerFirst = new Cricket();
+            _playerSecond = new Cricket();
+        }
+
+        [Given(@"PlayerFirst has scored (.*) runs")]
+        public void GivenPlayerFirstHasScoredRuns(int scores)
+        {
+            _playerFirst.Score(scores);
+        }
+
+        [Given(@"PlayerSecond has scored (.*) runs")]
+        public void GivenPlayerSecondHasScoredRuns(int scores)
+        {
+            _playerSecond.Score(scores);
+        }
+
+        [Given(@"PlayerFirst gets out")]
+        public void GivenPlayerFirstGetsOut()
+        {
+            _playerFirst.GetOut();
+        }
+        [Given(@"PlayerSecond gets out")]
+        public void GivenPlayerSecondGetsOut()
+        {
+            _playerSecond.GetOut();
+        }
+
+        [Then(@"PlayerSecond is winner")]
+        public void ThenPlayerSecondIsWinner()
+        {
+            _playerSecond.Winner(_playerFirst);
+            _playerSecond.isWinner.Should().Be(true);
+        }
+
+        [Then(@"PlayerFirst is winner")]
+        public void ThenPlayerFirstIsWinner()
+        {
+            _playerSecond.Winner(_playerFirst);
+            _playerFirst.isWinner.Should().Be(true);
+        }
+
+       
+
+
+        [Then(@"Match is tie")]
+        public void ThenMatchIsTie()
+        {
+
+            _playerSecond.Winner(_playerFirst);
+            _playerFirst.isWinner.Should().Be(true);
+            _playerSecond.isWinner.Should().Be(true);
+
+        }
+
+
+    }
+}
